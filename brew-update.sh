f61701ee965a2f7c42fd67d3a1ac47163e8ae16c
flyingice <flyingice.eth@outlook.com>
2018-09-30 15:19:50 +0200

    brew-update.sh: download cache cleanup

--- brew-update.sh
+++ brew-update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# software update via Homebrew
+# formula update via Homebrew
 BIN=brew
 if [ -n "$(which $BIN)" ]; then
   echo "Fetch the newest version of Homebrew from GitHub..."
   brew update
   echo "Upgrade outdated brews..."
-  brew upgrade --cleanup && echo "Done."
+  brew upgrade
+  echo "Remove old versions of installed formula..."
+  brew cleanup -s
+  echo "Done."
 else
   echo "$BIN: command not found. Please install $BIN."
   exit 1
